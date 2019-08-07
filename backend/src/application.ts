73fcff8347114ceaec3e6ec879a5197b67eb36f3
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-08-07 15:19:18 +0100

    feat: added cors to backend

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,10 +1,13 @@
+import * as cors from 'cors';
 import * as express from "express";
 import { apiController } from "./controllers/api";
 
+/* tslint:disable:no-var-requires */
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
 app.use(apiController);
 
 export default app;
