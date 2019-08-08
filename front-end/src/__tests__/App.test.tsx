8181ba5d639cf4fbd4623d3733ff19679df4fc56
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-08-08 21:43:07 +0100

    bug: fixed testing bug. NOTE: requires updating jestSetup.js file manually in node_modules

--- front-end/src/__tests__/App.test.tsx
+++ front-end/src/__tests__/App.test.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from '../App';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from '../App';
 import store from '@App/store';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+describe('Application', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
       ,
-    div);
+      div);
+  });
 });
